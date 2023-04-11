0dbc467c195dce4ae46f6a7c89f2384af9d30371
Thomas Madden <tmadden@mgh.harvard.edu>
2023-04-11 15:27:28 -0400

    Fix CI cmake commands

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,5 +5,5 @@ export CC=`which gcc-10`
 export CXX=`which g++-10`
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCRADLE_USE_CONAN=ON ..
 make websocket_server
