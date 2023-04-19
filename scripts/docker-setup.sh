2613fd064afccac82ff75d4fa7454f4306d96dc1
Thomas Madden <tmadden@mgh.harvard.edu>
2023-04-19 13:12:28 -0400

    Remove Docker build/release support

--- scripts/docker-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x -e
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y lcov ocaml-nox python3-pip cmake git g++-10
-pip3 install virtualenv
-gcc-10 --version
-./set-up-python.sh --python=python3
